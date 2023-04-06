61b29932fa90731a91414e11b627eccf2b4a7efc
coder <soumyodipsutra@gmail.com>
2023-04-06 21:30:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cryptotracer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.cryptotracer;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView currencyRV;
+    private EditText searchEdt;
+    private ArrayList<CurrencyModal> currencyModalArrayList;
+    private CurrencyRVAdapter currencyRVAdapter;
+    private ProgressBar loadingPB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchEdt = findViewById(R.id.idEdtCurrency);
+
+        // initializing all our variables and array list.
+        loadingPB = findViewById(R.id.idPBLoading);
+        currencyRV = findViewById(R.id.idRVcurrency);
+        currencyModalArrayList = new ArrayList<>();
+
+        // initializing our adapter class.
+        currencyRVAdapter = new CurrencyRVAdapter(currencyModalArrayList, this);
+
+        // setting layout manager to recycler view.
+        currencyRV.setLayoutManager(new LinearLayoutManager(this));
+
+        // setting adapter to recycler view.
+        currencyRV.setAdapter(currencyRVAdapter);
+
+        // calling get data method to get data from API.
+        getData();
+
+    }
+}
